71e6916d38e1bf560507314bcbc7b853e59471a4
zinker1999 <971758901@qq.com>
2019-04-10 07:42:33 +0800

    Static Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'staticpages/home'
+  get 'staticpages/help'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "application#hello"
 end
